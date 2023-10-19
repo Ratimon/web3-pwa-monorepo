fde1a71cc2cd34ab1adbd76f4385191cd137e545
Ratimon <ratixoxo@gmail.com>
2023-10-19 12:41:06 +0700

    Feat(./web): Adding connect wallet

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,2 +1,51 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import '../app.postcss';
+    import NavTabs from '$lib/components/daisyui/NavTabs.svelte';
+    import {name, description, themeColor, appleStatusBarStyle} from 'web-config';
+    import {url} from '$lib/utils/path';
+	import ConnectButton from '$lib/web3/ConnectButton.svelte';
+
+</script>
+
+<svelte:head>
+	<title>{name}</title>
+	<meta name="title" content={name} />
+	<meta name="description" content={description} />
+
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content={name} />
+	<meta property="og:description" content={description} />
+	<meta property="twitter:title" content={name} />
+	<meta property="twitter:description" content={description} />
+
+	<!-- minimal -->
+	<!-- TODO automatise -->
+	<!-- <link rel="icon" href={url('/pwa/favicon.svg')} type="image/svg+xml" />
+	<link rel="icon" href={url('/pwa/favicon.ico')} sizes="any" />32×32 -->
+	<link rel="apple-touch-icon" href={url('/pwa/apple-touch-icon.png')} /><!-- 180×180 -->
+
+	<!-- extra info -->
+	<meta name="theme-color" content={themeColor} />
+	<meta name="mobile-web-app-capable" content="yes" />
+	<meta name="application-name" content={name} />
+
+	<!-- apple -->
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content={appleStatusBarStyle} />
+	<meta name="apple-mobile-web-app-title" content={name} />
+</svelte:head>
+
+<div class="relative top-0 z-50 navbar bg-base-100 h-16 p-1 border-b-2 border-primary ">
+	<div class="flex-1 place-content-center" >
+		<NavTabs
+			pages={[
+				{pathname: '/', title: 'Home'},
+			]}
+		/>
+	</div>
+	<div class="flex-none">
+		<ConnectButton />
+	</div>
+</div>
+  
+<slot />
--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,10 +1,33 @@
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter()
-  },
-  preprocess: vitePreprocess()
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: [
+		vitePreprocess(),
+		preprocess({
+			// postcss make use of tailwind
+			// we ensure it get processed, see postcss.config.cjs
+			postcss: true,
+		}),
+	],
+	
+
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter(),
+		alias: {
+			// alias for web-config
+			'web-config': './src/web-config.json',
+			$data: './src/data',
+			$external: './src/external',
+		},
+	}
 };
+
 export default config;
